785777f48a75ec05fd7bf317f316c9310b83a090
hyu25 <angelyu19970627@gmail.com>
2022-08-07 10:49:04 -0400

    installedstripe

---
+++ server/api/checkout.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+const express = require('express')
+// const router = require('express').Router()
+const app = express()
+app.use(express.static('public'))
+
+const YOUR_DOMAIN = 'http://localhost:8080'
+
+app.post('/create-checkout-session:orderId', async (req, res) => {
+  const order = await Order.findById(req.params.orderId, {
+    include: {model: LineItem},
+  })
+  const session = await stripe.checkout.sessions.create({
+      Line_items:
+    // line_items: [
+    //   {
+    //     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+    //     price: '{{PRICE_ID}}',
+    //     quantity: 1,
+    //   },
+    // ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  })
+
+  res.redirect(303, session.url)
+})
+
+//   app.listen(8080, () => console.log('Running on port 8080'));
