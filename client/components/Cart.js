ee28dfa0a9c2e4687adddb98d722dbbd5eff2ba8
Eve <eveholzgruber@gmail.com>
2022-08-07 04:25:28 +0000

    add incomplete cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,13 +2,46 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-const Cart = () => {
-  return (
-    <div>
-      <h1>Cart</h1>
+// const Cart = () => {
+//   return (
+//     <div>
+//       <h1>Cart</h1>
       
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+export const Cart = ( { cart }) => {
+  const lineItems = cart.lineItems || [];
+  
+  return (
+    <ul>
+    {
+      lineItems.map(lineItem => {
+        return (
+        <li key={lineItem.id}>
+        {lineItem.product.title}
+        {lineItem.product.photoUrl}
+        ${lineItem.price}
+        Quantity: {lineItem.quantity}
+        </li>
+        )
+      })
+    }
+    </ul>
+    )
 }
 
-export default Cart
+const mapState = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatch = dispatch => {
+  //get cart
+  //add to cart
+  //remove from cart
+}
+
+export default connect(mapState, mapDispatch)(Cart)
